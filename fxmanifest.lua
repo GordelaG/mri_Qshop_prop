3a80ba78b381fa0d9e34937043200e43cbbb7d81
Gordela <ailtonplay2700@gmail.com>
2025-09-13 16:44:13 -0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game {'gta5'}
+lua54 'yes'
+version '1.0.0'
+this_is_a_map 'yes'
+
+author 'New Age Studios'
+description 'New Age Mri_Qshop'
+version '1.0.0'
+
+file {
+      'stream/**/*.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
